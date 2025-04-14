6e7b6296b7e4d51b3e78230c7ef0ddb6a839f749
itkomputer01 <itkomputer01@gmail.com>
2025-04-14 21:10:04 +0700

    perubahan jadi lebih panjang

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
-tesssss saTU
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>INI PERUBAHAN KE DUA</h1>
+</body>
+
+</html>
